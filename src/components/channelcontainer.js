279be4e3da1b51755e94423687d704944bd80975
AlonSchaffer <alon5285@gmail.com>
2022-01-27 22:52:54 +0200

    https://github.com/AlonSchaffer/LoopMachineAlon.git

--- src/components/channelcontainer.js
+++ src/components/channelcontainer.js
@@ -12,41 +12,17 @@ function ChannelContainer() {
     const [duration, setDuration] = useState(17);
 
     //reference to progressbar
-    const progressBar = useRef();
-
-    const animationRef = useRef();
 
 
     const handleToggle = () => {
-        const prevValue = playingState
-        setPlayingState(!prevValue);
-        if(!prevValue)
-        {
-            animationRef.current = requestAnimationFrame(whilePlaying)
-        }
-        else{
-            cancelAnimationFrame(animationRef.current)
-        }
+        setPlayingState(!playingState);
     }
 
-    const whilePlaying = ()=>{
-        progressBar.current.value= currentTime;
-        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration *100}%`)
-        setCurrentTime(progressBar.current.value)
-        
-    }  
     const handleLoop = () => {
         setLoopState(!loopState)
     }
 
-    const changeRange = () => {
-        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration *100}%`)
-        setCurrentTime(progressBar.current.value)
-    }
 
-    const getSeekHandler = (seek)=>{
-
-    }
 
     return <RowContainer>
         <div className='controlPanel'>
@@ -61,13 +37,8 @@ function ChannelContainer() {
         <div className='channelsPanel'>
             <h2>Channel Clips:</h2>
             <h5>{(loopState) ? 'loop mode is active' : ''}</h5>
-            <RowContainer>
-            <label htmlFor='progressBar'>{currentTime}</label>
-            <input type='range' name='progressBar' className='progressbar' min='0' max={duration ? duration.toFixed(2) : 0}
-            step='0.1' value={currentTime} ref={progressBar} onChange={changeRange}/>
-            </RowContainer>
             {UrlData.map((data, index) => {
-                return <Channel url={data.url} getseekfromchild={getSeekHandler} name={data.name} key={index} playingState={playingState} loopState={loopState} currentTimeState= {currentTime} />
+                return <Channel url={data.url} name={data.name} key={index} playingState={playingState} loopState={loopState} currentTimeState= {currentTime} />
 
             })}
         </div>
